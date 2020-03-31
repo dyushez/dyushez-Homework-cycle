3d77ce3f083235b3c8e17d2785673e3f1fd8b5be
dyushez <51192331+dyushez@users.noreply.github.com>
2020-03-31 23:10:31 +0300

    Update database_processing.py

--- database_processing.py
+++ database_processing.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
-data1 = pd.read_csv('')
+movies = pd.read_csv('movies.csv', index_col='movieId')
+tags = pd.read_csv('tags.csv', index_col='movieId')
 
+# merge movies and tags data frames in on movieId
+data_tags_movies = pd.merge(tags, movies, on='movieId')
+
+print(data_tags_movies)
